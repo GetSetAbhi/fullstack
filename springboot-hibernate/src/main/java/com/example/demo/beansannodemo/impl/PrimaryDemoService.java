79e0b71517baca63bcd2b4432b475b431f70cb64
Raina <ar52540@bose.com>
2021-04-11 17:27:19 +0530

    Added code for bean lifecycle

--- springboot-hibernate/src/main/java/com/example/demo/beansannodemo/impl/PrimaryDemoService.java
+++ springboot-hibernate/src/main/java/com/example/demo/beansannodemo/impl/PrimaryDemoService.java
@@ -1,5 +1,7 @@
 package com.example.demo.beansannodemo.impl;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -7,11 +9,27 @@ import com.example.demo.beansannodemo.DemoService;
 
 @Service("primaryDemoService")
 @Primary
-public class PrimaryDemoService implements DemoService {
+public class PrimaryDemoService implements DemoService, InitializingBean, DisposableBean  {
 
 	@Override
 	public void hello() {
 		System.out.println("PRIMARY");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("##########################");
+		System.out.println("Destroying bean of class " + this.getClass().getSimpleName());
+		System.out.println("##########################");
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("##########################");
+		System.out.println("Created bean of class " + this.getClass().getSimpleName());
+		System.out.println("##########################");
+		
 	}
 
 }
