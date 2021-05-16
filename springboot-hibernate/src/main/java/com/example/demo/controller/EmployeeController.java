1abb7476c1eb4f277b74a17058b04827e2911ae4
Raina <ar52540@bose.com>
2021-05-17 00:31:45 +0530

    Request Param

--- springboot-hibernate/src/main/java/com/example/demo/controller/EmployeeController.java
+++ springboot-hibernate/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -35,7 +36,7 @@ public class EmployeeController {
 	}
 	
 	@GetMapping(path = "/employee/{id}/emails")
-	public ResponseEntity<Emails> getEmployeeEmails(@PathVariable Long id) {
+	public ResponseEntity<Emails> getEmployeeEmails(@PathVariable Long id, @RequestParam(name = "bro", defaultValue = "bro") String bro) {
 		Employee e = service.getEmployeeById(id);
 		List<Email> mails = e.getEmails();
 		Emails emails = new Emails();
